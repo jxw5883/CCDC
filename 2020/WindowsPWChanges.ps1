6e4f55d0e9a8ac65a6bbe1906f1fbe2f85684a40
Brant Goings <bgraulph@gmail.com>
2020-02-06 15:33:34 -0500

    Brant comment

--- 2020/WindowsPWChanges.ps1
+++ 2020/WindowsPWChanges.ps1
@@ -1,1 +1,5 @@
+# Powershell to check last password change per users - Ellie Rennard
+
+# Sort by latest password change? Or filter by password change?
+# Needs relative user Desktop, not absolute
 ﻿Get-LocalUser -Name * | Select-Object Name, Enabled, PasswordChangeableDate, PasswordExpires, UserMayChangePassword, PasswordRequired, PasswordLastSet, LastLogon | out-file -FilePath "C:\Users\Ellie Rennard\Desktop\pwchanges.txt"
