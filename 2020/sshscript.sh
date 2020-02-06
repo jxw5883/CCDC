88daa3defc0f18851f28625744fe20bcc9b6f8b2
mobiusKey <wallacemark77@gmail.com>
2020-02-06 13:07:32 -0500

    added failed ssh attempts script

---
+++ 2020/sshscript.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "running..." > /dev/tty
+while :
+do
+  logFile=$(cat /var/log/auth.log | grep "authentication failure")
+  knownFailures=$(cat ./knownFailures)
+	echo "checking failures..." > /dev/tty
+        output=$(diff <(echo "$logFile") <(echo "$knownFailures"))
+        echo "$output" > /dev/tty
+echo "saving results..." > /dev/tty
+echo "$logFile" > ./knownFailures
+sleep 5
+
+done
