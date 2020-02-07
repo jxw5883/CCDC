2926bb75baa6c0bab60a8bda791e5c9c155d1c7b
Brant Goings <bgraulph@gmail.com>
2020-02-06 19:16:40 -0500

    Comment and author

--- 2020/notify_login_install.sh
+++ 2020/notify_login_install.sh
@@ -1,6 +1,5 @@
+#!/bin/bash
 # Notify successful login - Petr Esakov
-
-# Automate with cron/watch?
 
 echo "notify-send -t 3000 \"User logged in\" \`whoami\`" > /etc/profile.d/notify_log.sh
 echo "wall -n \"User logged in\" \`whoami\`" >> /etc/profile.d/notify_log.sh
