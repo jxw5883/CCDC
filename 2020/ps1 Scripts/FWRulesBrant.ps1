81fa7f6d47adf3033daf9b8dac2dd7bd3ef288ca
Brant Goings <bgraulph@gmail.com>
2020-02-24 11:45:18 -0500

    Updates

--- 2020/ps1 Scripts/FWRulesBrant.ps1
+++ 2020/ps1 Scripts/FWRulesBrant.ps1
@@ -5,10 +5,28 @@ Get-NetFirewallProfile
 Get-NetFirewallProfile -Name Public | Get-NetFirewallRule
 
 # This doesn't work
-Get-NetFirewallRule -Enabled True | Get-NetFirewallProfile -Name Public
+Get-NetFirewallRule -Enabled True #| Get-NetFirewallProfile -Name Public
+(Get-NetFirewallRule).Enabled # Returns values of property of object
+
 
 # or this
 Get-NetFirewallProfile -Name Public | Get-NetFirewallRule -Enabled True
+
+Get-NetFirewallRule | Where-Object {$_.Enabled -like "True"}
+
+Get-NetFirewallRule | Where-Object {$_.Description -like "80"} # Finds more specific stuff too
+
+Get-NetFirewallRule | Where-Object {$_.Description -like "*80*"} | ft DisplayName, Profile, Enabled
+Get-NetFirewallRule | Where-Object {$_.Description -eq "*80*"} | ft DisplayName, Profile, Enabled
+
+Get-NetFirewallRule | ft Name, DisplayGroup
+
+Disable-NetFirewallRule
+
+# | outfile -FilePath doc.txt
+
+
+Get-NetConnectionProfile
 
 # Decent stuff found here: https://devblogs.microsoft.com/scripting/learn-how-to-use-powershell-to-parse-the-firewall-log/
 # Talks about narrowing log files down to hits per rule
@@ -18,3 +36,5 @@ netsh advfirewall show allprofiles | Select-String Filename
 # Checking a domain's F/W status: https://isc.sans.edu/forums/diary/Powershell+Active+Directory+and+the+Windows+Host+Firewall/24706/
 
 Set-NetFirewallProfile -name domain -LogMaxSizeKilobytes 10240 -LogAllowed true -LogBlocked true
+
+# import-csv, get-member
